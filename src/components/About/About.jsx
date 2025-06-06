05a933de37b94e667c22402c71606ce5e3da2952
Mark Taratynov <mark.taratynov@gmail.com>
2025-06-06 14:42:15 +0200

    Refactor About component layout for improved readability and structure

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,19 +1,32 @@
+import MenuButton from "../StartMenu/MenuButton";
+import { useNavigate } from "react-router-dom";
 export default function About() {
+  const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-4">
-      <h1 className="text-2xl font-bold">About This Game</h1>
-      <p className="text-gray-700">
-        This game is a fun and interactive way to learn about Pokémon. You can
-        explore various Pokémon, their stats, and even battle them in a
-        simulated environment.
-      </p>
-      <p className="text-gray-700">
-        Developed by [Your Name], this game aims to provide an engaging
-        experience for Pokémon fans and newcomers alike.
-      </p>
-      <p className="text-gray-700">
-        For more information, visit the <a href=""></a>
-      </p>
+    <div className="flex flex-col items-center min-h-screen justify-center gap-6">
+      <div className="flex md:max-w-[50rem] flex-col items-center justify-center gap-6 p-4">
+        <h1 className="text-2xl font-bold">About This Game</h1>
+        <p className="text-gray-700 text-center">
+          This game is a fun and interactive way to learn about Pokémon. You can
+          explore various Pokémon, their stats, and even battle them in a
+          simulated environment.
+        </p>
+        <p className="text-gray-700 text-center">
+          Developed by Mark Taratynov, this game aims to provide an engaging
+          experience for Pokémon fans and newcomers alike.
+        </p>
+        <p className="text-gray-700">
+          For more information, visit the repository{" "}
+          <a
+            className="text-blue-500"
+            href="https://github.com/Prostecki/pokemon-app"
+            target="_blank"
+          >
+            Pokemon App
+          </a>
+        </p>
+      </div>
+      <MenuButton onClick={() => navigate("/")}>Back to menu</MenuButton>
     </div>
   );
 }
