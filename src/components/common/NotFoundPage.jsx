eb5d454b758ec08c743215172ff39334ccf82596
Mark Taratynov <mark.taratynov@gmail.com>
2025-06-12 17:47:25 +0200

    feat: Update NotFoundPage message to indicate upcoming features

--- src/components/common/NotFoundPage.jsx
+++ src/components/common/NotFoundPage.jsx
@@ -6,7 +6,9 @@ export default function NotFoundPage() {
   };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-200 to-yellow-100 gap-10">
-      <h1>404 - Page is not found</h1>
+      <h1 className="text-4xl font-bold drop-shadow-md text-gray-800/90">
+        New features are coming soon, stay tuned!
+      </h1>
       <button onClick={handleBackToMenu} className="menu-buttons">
         Back to menu
       </button>
