837e55ae2c7f5be34ba74e03ff5e6ce129271c30
Mark Taratynov <mark.taratynov@gmail.com>
2025-06-19 20:01:13 +0200

    feat: Add styling for About component with CSS classes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,32 +1,33 @@
 import MenuButton from "../common/MenuButton";
 import { useNavigate } from "react-router-dom";
 import { motion } from "framer-motion";
+import "./about.css";
 
 export default function About() {
   const navigate = useNavigate();
   return (
-    <div className="flex flex-col items-center min-h-screen justify-center gap-6">
+    <div className="about-container">
       <motion.div
-        className="flex md:max-w-[50rem] flex-col items-center justify-center gap-6 p-4"
+        className="about-content"
         initial={{ opacity: 0, y: 40 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -40 }}
         transition={{ duration: 0.4 }}
       >
-        <h1 className="text-2xl font-bold">About This Game</h1>
-        <p className="text-gray-700 text-center">
+        <h1 className="about-title">About This Game</h1>
+        <p className="about-paragraph-center">
           This game is a fun and interactive way to learn about Pokémon. You can
           explore various Pokémon, their stats, and even battle them in a
           simulated environment.
         </p>
-        <p className="text-gray-700 text-center">
+        <p className="about-paragraph-center">
           Developed by Mark Taratynov, this game aims to provide an engaging
           experience for Pokémon fans and newcomers alike.
         </p>
-        <p className="text-gray-700">
+        <p className="about-paragraph">
           For more information, visit the repository{" "}
           <a
-            className="text-blue-500"
+            className="about-link"
             href="https://github.com/Prostecki/pokemon-app"
             target="_blank"
           >
